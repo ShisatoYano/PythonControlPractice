5de94752f9fcaafdee6ea77ea44c64afe6bb32de
Shisato <shisatoyano@gmail.com>
2020-04-03 00:00:00 +0900

    define main function.

--- TwoWheelModel/two_wheel_model.py
+++ TwoWheelModel/two_wheel_model.py
@@ -5,6 +5,45 @@ author: Shisato Yano (@4310sy)
 """
 
 from control.matlab import *
+import numpy as np
 
-sys2tf = tf([1, 0.5], [1, 5])
-print(sys2tf)
+class TwoWheelModel:
+
+    def __init__(self, tr, td):
+        """
+        Initialize two wheel model
+
+        tr: tire radius [m]
+        td: tread [m]
+        """
+
+        self.tire_radius_m = tr
+        self.tread_m = td
+
+    def calculate_input(self, tsr, tsl):
+        """
+        tsr: tire speed right [m/s]
+        tsl: tire speed left [m/s]
+        """
+
+        mat = np.ndarray([[self.tire_radius_m/2, self.tire_radius_m/2],
+                         [self.tire_radius_m/self.tread_m, -self.tire_radius_m/self.tread_m]])
+
+        vec = np.ndarray([[tsr], [tsl]])
+
+        input_vec = np.dot(mat, vec)
+
+        return input_vec
+
+def main():
+    print("Run " + __file__)
+
+    # set parameters
+    tire_radius_m = 0.08
+    tread_m = 0.26
+
+    # initialize
+    two_wheel = TwoWheelModel(tire_radius_m, tread_m)
+
+if __name__ == '__main__':
+    main()
