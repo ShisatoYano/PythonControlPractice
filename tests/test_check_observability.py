fbe20eb500d3253a21a852e33d2910da0269bcba
Shisato <shisatoyano@gmail.com>
2020-04-17 21:24:02 +0900

    add test_check_observability.py.

---
+++ tests/test_check_observability.py
@@ -0,0 +1,17 @@
+import unittest
+import sys
+import os
+sys.path.append(os.path.dirname(__file__) + "..")
+try:
+    from Observability import check_observability as m
+except:
+    raise
+
+class Test(unittest.TestCase):
+
+    def test1(self):
+        m.show_result = False
+        m.main()
+
+if __name__ == '__main__':
+    unittest.main()
